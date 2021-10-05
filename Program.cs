76c66508842ec3b274434bf3b5219c549a45c415
Minh Tri <42665212+Mtris7@users.noreply.github.com>
2021-10-05 23:24:18 +0700

    SubArray

--- Program.cs
+++ Program.cs
@@ -195,5 +195,25 @@ namespace LeetCode
         {
             return ((i >> j) & 1) == 1;
         }
+
+        //SubArray
+        public int SubArray(int[] nums, int s)
+        {
+            int count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == s)
+                {
+                    count++;
+                    continue;
+                }
+                else
+                {
+                    for (int j = i + 1; j < nums.Length; j++)
+                        if (s - nums[i] == nums[j]) count++;
+                }
+            }
+            return count;
+        }
     }
 }
