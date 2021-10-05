5cded22e731ab73a182f9f0a0ce788c86934051b
Minh Tri <42665212+Mtris7@users.noreply.github.com>
2021-10-05 14:10:00 +0700

    done

---
+++ NumArray.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class NumArray
+    {
+        //range-sum-query-immutable
+
+        //C1:
+        //private int[] array;
+        //public NumArray(int[] nums)
+        //{
+        //    array = nums;
+        //}
+
+        //public int SumRange(int left, int right)
+        //{
+        //    int sum = 0;
+        //    if (left <= right)
+        //    {
+        //        for (; left <= right; left++)
+        //        {
+        //            sum += array[left];
+        //        }
+        //    }
+        //    return sum;
+        //}
+
+        //C2:
+        private int[] array;
+        public NumArray(int[] nums)
+        {
+            array = new int[nums.Length];
+            array[0] = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                array[i] = array[i - 1] + nums[i];
+            }
+        }
+
+        public int SumRange(int left, int right)
+        {
+            if (left == 0) return array[right];
+            else if (left <= right)
+                return array[right] - array[left - 1];
+            return 0;
+        }
+    }
+}
---
+++ NumMatrix.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class NumMatrix
+    {
+        private int[][] _matrix;
+        public NumMatrix(int[][] matrix)
+        {
+            _matrix = matrix;
+            for (int i = 0; i < matrix.Length; i++)
+                for (int j = 1; j < matrix[i].Length; j++)
+                    _matrix[i][j] += _matrix[i][j - 1];
+        }
+
+        public int SumRegion(int row1, int col1, int row2, int col2)
+        {
+            int sum = 0;
+            for (; row1 <= row2; row1++)
+            {
+                if (col1 == 0)
+                    sum += _matrix[row1][col2];
+                else
+                    sum += _matrix[row1][col2] - _matrix[row1][col1 - 1];
+            }
+
+            return sum;
+        }
+    }
+}
