4992b62571f6162b17a98cd87b97110cb47d0ddb
Minh Tri <minhtri.win@gmail.com>
2021-10-16 11:06:12 +0700

    Addition

--- Lesson4/Lesson4/Program.cs
+++ Lesson4/Lesson4/Program.cs
@@ -163,6 +163,24 @@ namespace Lesson4
             }
             return sum;
         }
+        //266. Palindrome Permutation
+        public bool CanPermutePalindrome(string s)
+        {
+            /* create hashtable to save value of array and number of the occurrences
+             *  if array and number of the occurrences = 2 => remove 
+             *  return true if hashtable just has 1 element or don't have
+             */
+            var dic = new Dictionary<char, int>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!dic.ContainsKey(s[i]))
+                    dic.Add(s[i], 1);
+                else
+                    dic[s[i]]++;
+                if (dic[s[i]] % 2 == 0) dic.Remove(s[i]);
+            }
+            return !dic.Where(x => x.Value == 1).Any() || dic.Where(x => x.Value == 1).Count() == 1;
+        }
 
         //Addtion
         //length-of-last-word
