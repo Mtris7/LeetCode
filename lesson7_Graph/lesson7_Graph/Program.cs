75efa350e180588e3a031c45e39c6790b57b64b9
Minh Tri <minhtri.win@gmail.com>
2021-10-26 17:23:37 +0700

    lesson 7 Graph

--- lesson7_Graph/lesson7_Graph/Program.cs
+++ lesson7_Graph/lesson7_Graph/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace lesson7_Graph
 {
@@ -7,9 +9,141 @@ namespace lesson7_Graph
         static void Main(string[] args)
         {
             //FindCircleNum(new int[3][] { new int[]{ 1, 1, 0 }, new int[] { 1, 1, 0 }, new int[] { 0, 0, 1 } });
+
+            //[[1],[2],[3],[]]
+            //IList<IList<int>> rooms = new List<IList<int>>();
+            //List<int> list = new List<int>();
+            //list.Add(1);
+            //rooms.Add(list);
+            //list = new List<int>();
+            //list.Add(2);
+            //rooms.Add(list);
+            //list = new List<int>();
+            //list.Add(3);
+            //rooms.Add(list);
+            //list = new List<int>();
+            //rooms.Add(list);
+            //CanVisitAllRooms(rooms);
+            //NumIslands(new char[4][] { new char[]{ '1', '1', '0', '0','0' },
+            //                           new char[] { '1', '1', '0' , '0', '0'}, 
+            //                           new char[]{ '0', '0', '1', '0','0' },
+            //                           new char[] { '0', '0', '0', '1',  '1' }});
+
+            //how to test treeNode
+            //root = [0,2,4,1,null,3,-1,5,1,null,6,null,8]
+            //var root = new TreeNode(0);
+            //var cur = root;
+            //cur.left = new TreeNode(2);
+            //cur.right = new TreeNode(4);
+            //cur = cur.left;
+            //cur.left = new TreeNode(1);
+            //cur = root.right;
+            //cur.left = new TreeNode(3);
+            //cur.right = new TreeNode(-1);
+            //Solution.SumOfLeftLeaves(root);
+
+
             Console.WriteLine("Hello World!");
+            //Solution
+            //695. Max Area of Island
+            //200. Number of Islands
+            //841. Keys and Rooms
+            //547. Number of Provinces
+            //404.Sum of Left Leaves
         }
 
+        //###########################################################################################################
+        //695. Max Area of Island
+        private int count = 0;
+        private int max = 0;
+        public int MaxAreaOfIsland(int[][] grid)
+        {
+            bool[][] visited = new bool[grid.Length][];
+            for (int i = 0; i < grid.Length; i++)
+                visited[i] = new bool[grid[i].Length];
+
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[0].Length; j++)
+                {
+                    if (!visited[i][j] && grid[i][j] == 1)
+                    {
+                        if (max < count) max = count;
+                        count = 0;
+                        DFS(i, j, grid, visited);
+                    }
+                }
+            }
+            if (max < count) max = count;
+            return max;
+        }
+        private void DFS(int row, int column, int[][] grid, bool[][] visited)
+        {
+            count++;
+            visited[row][column] = true;
+            for (int i = 0; i < 4; i++)
+            {
+                int r = dr[i] + row;
+                int c = dc[i] + column;
+                if (r >= 0 && r < grid.Length && c >= 0 && c < grid[0].Length
+                   && !visited[r][c] && grid[r][c] == 1)
+                    DFS(r, c, grid, visited);
+            }
+        }
+        //###########################################################################################################
+        //200. Number of Islands
+
+        private int[] dr = new int[] { 0, 1, 0, -1 };
+        private int[] dc = new int[] { 1, 0, -1, 0 };
+        public int NumIslands(char[][] grid)
+        {
+            bool[][] visited = new bool[grid.Length][];
+            for (int i = 0; i < grid.Length; i++)
+                visited[i] = new bool[grid[i].Length];
+            int count = 0;
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[0].Length; j++)
+                {
+                    if (!visited[i][j] && grid[i][j] == '1')
+                    {
+                        count++;
+                        DFS(i, j, grid, visited);
+                    }
+                }
+            }
+            return count;
+        }
+        private void DFS(int row, int column, char[][] grid, bool[][] visited)
+        {
+            visited[row][column] = true;
+            for (int i = 0; i < 4; i++)
+            {
+                int r = dr[i] + row;
+                int c = dc[i] + column;
+                if (r >= 0 && r < grid.Length && c >= 0 && c < grid[0].Length
+                   && !visited[r][c] && grid[r][c] == '1')
+                    DFS(r, c, grid, visited);
+            }
+        }
+        //#############################################################################################################
+        //841. Keys and Rooms
+        //Create DFSVisitRoom to
+        public bool CanVisitAllRooms(IList<IList<int>> rooms)
+        {
+            bool[] visited = new bool[rooms.Count];
+            DFSVisitRoom(0, rooms, visited);
+            return visited.Where(x => x == true).ToList().Count == rooms.Count;
+        }
+        private void DFSVisitRoom(int startNode, IList<IList<int>> rooms, bool[] visited)
+        {
+            visited[startNode] = true;
+            for (int i = 0; i <= rooms[startNode].Count - 1; i++)
+            {
+                if (!visited[rooms[startNode][i]])
+                    DFSVisitRoom(rooms[startNode][i], rooms, visited);
+            }
+        }
         //547. Number of Provinces
 
         /* create list "visited" is list bool equal number of point in array
---
+++ lesson7_Graph/lesson7_Graph/TreeNode.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lesson7_Graph
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public class Solution
+    {
+        //
+        int count = 1;
+        public int MinDepth(TreeNode root)
+        {
+            if (root == null) return 0;
+            if (root.left == null && root.right == null) return 1;
+            int leftSum = DFS(root.left);
+            count = 1;
+            int rightSum = DFS(root.right);
+            if (leftSum < rightSum)
+                return leftSum;
+            else
+                return rightSum;
+        }
+        int DFS(TreeNode root)
+        {
+            if (root == null) return count;
+            count++;
+            if (root.left == null && root.right == null)
+                return count;
+
+            int leftSum = DFS(root.left);
+
+            int rightSum = DFS(root.right);
+            if (leftSum < rightSum)
+                return leftSum;
+            else
+                return rightSum;
+        }
+        //########################################################################################
+        /// <summary>
+        /// 404. Sum of Left Leaves
+        /// </summary>
+        int sum = 0;
+        public int SumOfLeftLeaves(TreeNode root)
+        {
+            if (root.left == null && root.right == null) return 0;
+            DFS(root.left, true);
+            DFS(root.right, false);
+            return sum;
+        }
+        private void DFS(TreeNode root, bool checkLeft)
+        {
+            if (root == null) return;
+            if (checkLeft && root.left == null && root.right == null)
+                sum += root.val;
+
+            DFS(root.left, true);
+            DFS(root.right, false);
+        }
+    }
+}
