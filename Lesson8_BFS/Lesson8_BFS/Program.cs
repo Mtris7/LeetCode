269b93fb9633b2688754944d6ca36046a51dc53f
Minh Tri <minhtri.win@gmail.com>
2021-11-02 11:35:09 +0700

    add

--- Lesson8_BFS/Lesson8_BFS/Program.cs
+++ Lesson8_BFS/Lesson8_BFS/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Lesson8_BFS
 {
@@ -18,10 +19,10 @@ namespace Lesson8_BFS
             //[[0,0,0],[1,1,0],[1,1,0]]
             //[[0,0,0],[1,1,0],[1,1,1]]
             var grid = new int[1][] { new int[1] { 0 }};
-            //
-            ///
-            //
-            MinMutation("AACCGGTT", "AAACGGTA", new string[] { "AACCGGTA", "AACCGCTA", "AAACGGTA" });
+            //MinMutation("AACCGGTT", "AAACGGTA", new string[] { "AACCGGTA", "AACCGCTA", "AAACGGTA" });
+            //LadderLength("hit", "cog", new List<string> { "hot", "dot", "dog", "lot", "log", "cog" });
+
+
             //429.N - ary Tree Level Order Traversal
             //102. Binary Tree Level Order Traversal
             //107. Binary Tree Level Order Traversal II
@@ -36,6 +37,60 @@ namespace Lesson8_BFS
         //#########################################################################################################
         //#########################################################################################################
         //#########################################################################################################
+        /// <summary>
+        /// 127. Word Ladder
+        /// </summary>
+        /// <param name="beginWord"></param>
+        /// <param name="endWord"></param>
+        /// <param name="wordList"></param>
+        /// <returns></returns>
+        public int LadderLength(string beginWord, string endWord, IList<string> wordList)
+        {
+            var wordSet = new HashSet<string>(wordList);
+            if (!wordSet.Contains(endWord))
+                return 0;
+
+            var queue = new Queue<string>();
+            queue.Enqueue(beginWord);
+            var result = 1;
+
+            while (queue.Any())
+            {
+                var queueSize = queue.Count;
+                for (int i = 0; i < queueSize; i++) // size queue
+                {
+                    var current = queue.Dequeue();
+                    wordSet.Remove(current);
+                    var wordArray = current.ToArray();
+
+                    for (int m = 0; m < current.Length; m++)// m => length
+                    {
+                        var curChar = current[m];
+                        for (char n = 'a'; n <= 'z'; n++)//24
+                        {
+                            if (wordArray[m] != n)
+                            {
+                                wordArray[m] = n;
+                                var nextWord = new string(wordArray);
+
+                                if (nextWord == endWord)
+                                    return result + 1;
+
+                                if (wordSet.Contains(nextWord))
+                                {
+                                    queue.Enqueue(nextWord);
+                                    wordSet.Remove(nextWord);
+                                }
+                            }
+                            wordArray[m] = curChar;
+                        }
+                    }
+
+                }
+                result++;
+            }
+            return 0;
+        }
         //#########################################################################################################
         /// <summary>
         /// 433. Minimum Genetic Mutation
