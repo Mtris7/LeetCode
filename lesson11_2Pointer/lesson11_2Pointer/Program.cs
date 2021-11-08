fdc0f899abcee7adce6b30fbcb148fb6687473a5
Minh Tri <minhtri.win@gmail.com>
2021-11-08 17:04:19 +0700

    update new  713. Subarray Product Less Than K

--- lesson11_2Pointer/lesson11_2Pointer/Program.cs
+++ lesson11_2Pointer/lesson11_2Pointer/Program.cs
@@ -8,6 +8,7 @@ namespace lesson11_2Pointer
     {
         static void Main(string[] args)
         {
+            //NumSubarrayProductLessThanK(new int[] { 10, 5, 2, 1,2,11 },100);
             //LengthOfLongestSubstring("bbtablud");
             //FourSum(new int[] { 2, 2, 2, 2, 2 }, 8);
             //240. Search a 2D Matrix II
@@ -19,6 +20,89 @@ namespace lesson11_2Pointer
             //845.Longest Mountain in Array
             Console.WriteLine("Hello World!");
         }
+        //##########################################################################################################################
+        //##########################################################################################################################
+        /// <summary>
+        /// 713. Subarray Product Less Than K
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+
+        public int NumSubarrayProductLessThanK(int[] nums, int k)
+        {
+            if (nums == null || k == 0 || k == 1)
+            {
+                return 0;
+            }
+
+            int result = 1;
+            int i = 0;
+            int len = 0;
+            int count = 0;
+            while (i < nums.Length)
+            {
+                result *= nums[i]; //[10,5,2,1,2, 11]
+                if (result >= k)
+                {
+                    while (result >= k)
+                    {
+                        result /= nums[i - len];// when len = 1 => result = 1
+                                                //when len = 2 => result = 3 = result + len
+                        len--;                  //len = 3=> result = 6 = result + len
+                                                //len = 4 => result = 10 = result + len
+                    }
+                }
+                count += ++len;
+                i++;
+            }
+            return count;
+        }
+        // C1:
+        //public int NumSubarrayProductLessThanK(int[] nums, int k)
+        //{
+        //    if (nums.Length == 1 && nums[0] < k) return 1;
+        //    int i = 0;
+        //    int j = i + 1;
+        //    int result = 0;
+        //    int multi = nums[0];
+        //    var list = new HashSet<int>();
+        //    while (i < j) //[10,5,1,10]
+        //    {
+        //        multi *= nums[j];
+        //        if (multi < k)
+        //        {
+        //            if (j == nums.Length - 1)
+        //            {
+        //                if (j - i == 0) return result + 1; // 1 => 1 ; 1 2 => 3 ;  1 2 3 => 6 ; 1 2 3 4 => 10
+        //                else if (j - i == 1) return result + 3;
+        //                else
+        //                {
+        //                    while (i < j - 1)
+        //                    {
+        //                        result += (j - i);
+        //                        i++;
+        //                    }
+        //                    return result + 3;
+        //                }
+        //            }
+        //            result++;
+        //            if (j < nums.Length - 1)
+        //                j++;
+        //        }
+        //        else
+        //        {
+        //            if (nums[i] < k) result++;
+        //            i++;
+        //            multi = nums[i];
+        //            if (j <= nums.Length - 1 && i < nums.Length - 1)
+        //                j = i + 1;
+        //        }
+
+        //    }
+        //    if (nums[nums.Length - 1] < k) result++;
+        //    return result;
+        //}
         //##########################################################################################################################
         /// <summary>
         /// 3. Longest Substring Without Repeating Characters
