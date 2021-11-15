7adc97a968c01fe8fed8453d0a295408289ed7d0
Minh Tri <minhtri.win@gmail.com>
2021-11-15 09:49:55 +0700

    update

--- lesson11_Heap_PriorityQueue/lesson11_Heap_PriorityQueue/heap/1753.cs
+++ lesson11_Heap_PriorityQueue/lesson11_Heap_PriorityQueue/heap/1753.cs
@@ -9,26 +9,46 @@ namespace lesson11_Heap_PriorityQueue.heap
     {
         public int MaximumScore(int a, int b, int c)
         {
+            //c1:
+            //int result = 0;
+            //var dic = new Dictionary<int, int>();
+            //dic.Add(1, a);
+            //dic.Add(2, b);
+            //dic.Add(3, c);
+            //while (true)
+            //{
+            //    var max1 = dic.Values.Max();
+            //    var item1 = dic.First(x => x.Value == max1).Key;
+            //    dic.Remove(item1);
+            //    var max2 = dic.Values.Max();
+            //    var item2 = dic.First(x => x.Value == max2).Key;
+            //    dic.Remove(item2);
+            //    if (max1 < 1 || max2 < 1) return result;
+            //    max1--;
+            //    max2--;
+            //    dic.Add(item1, max1);
+            //    dic.Add(item2, max2);
+            //    result++;
+            //}
+
+            //C2: heap
+            var heap = new Heap<int>(HeapType.MaxHeap);
+            heap.Push(a);
+            heap.Push(b);
+            heap.Push(c);
             int result = 0;
-            var dic = new Dictionary<int, int>();
-            dic.Add(1, a);
-            dic.Add(2, b);
-            dic.Add(3, c);
-            while (true)
+            while(heap != null)
             {
-                var max1 = dic.Values.Max();
-                var item1 = dic.First(x => x.Value == max1).Key;
-                dic.Remove(item1);
-                var max2 = dic.Values.Max();
-                var item2 = dic.First(x => x.Value == max2).Key;
-                dic.Remove(item2);
+                var max1 = heap.Pop();
+                var max2 = heap.Pop();
                 if (max1 < 1 || max2 < 1) return result;
                 max1--;
                 max2--;
-                dic.Add(item1, max1);
-                dic.Add(item2, max2);
+                heap.Push(max1);
+                heap.Push(max2);
                 result++;
             }
+            return result;
         }
     }
 }
