439e0465605b7bcd47e3646932f61d07667eb168
Minh Tri <minhtri.win@gmail.com>
2021-11-18 10:02:51 +0700

    update new 23 leetcode

---
+++ lesson11_Heap_PriorityQueue/lesson11_Heap_PriorityQueue/heap/23.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lesson11_Heap_PriorityQueue
+{
+    public class _23
+    {
+        public ListNode MergeKLists(ListNode[] lists)
+        {
+            var heap = new HeapListNode();
+            var result = new ListNode(0);
+            var cur = result;
+            foreach (var item in lists)
+            {
+                if (item != null)
+                    heap.Push(item);
+            }
+            while (!heap.IsEmpty())
+            {
+                cur.next = heap.Pop();
+                cur = cur.next;
+                if (cur.next != null)
+                    heap.Push(cur.next);
+            }
+            return result.next;
+        }
+    }
+}
