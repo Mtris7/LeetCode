890dc7a4c890fef27f2a92bf367fe2a8c0950453
Minh Tri <42665212+Mtris7@users.noreply.github.com>
2021-11-29 22:38:19 +0700

    finish 1099 two-sum-less-than-k

---
+++ LeetCode/lesson11/2Pointer/1099.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.lesson11._2Pointer
+{
+    class _1099
+    {
+        //https://leetcode.com/problems/two-sum-less-than-k/
+        public int TwoSumLessThanK(int[] nums, int k)
+        {
+            Array.Sort(nums);
+            int l = 0;
+            int r = nums.Length - 1;
+            int result = int.MinValue;
+            while (l < r)
+            {
+                var sum = nums[l] + nums[r];
+
+                if (sum >= k)
+                    r--;
+                else if (sum < k)
+                {
+                    l++;
+                    result = Math.Max(result, sum);
+                }
+            }
+            if (result == int.MinValue) return -1;
+
+            return result;
+        }
+    }
+}
