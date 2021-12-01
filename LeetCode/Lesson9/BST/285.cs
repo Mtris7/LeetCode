e678089cc3e8d3f78c8df1de734da96e3303c2ab
Minh Tri <42665212+Mtris7@users.noreply.github.com>
2021-12-01 17:21:54 +0700

    add new 285

---
+++ LeetCode/Lesson9/BST/285.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    class _285
+    {
+        //https://leetcode.com/problems/inorder-successor-in-bst/
+        public TreeNode InorderSuccessor(TreeNode root, TreeNode p)
+        {
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            TreeNode result = null;
+            while (queue.Any())
+            {
+                var node = queue.Dequeue();
+                if (node.val <= p.val)
+                {
+                    if (node.right != null) queue.Enqueue(node.right);
+                }
+                else
+                {
+                    if ((result == null && node.val != p.val) || (result != null && result.val > node.val))
+                        result = node;
+                    if (node.left != null && node.val > p.val) queue.Enqueue(node.left);
+                }
+            }
+            return result;
+
+        }
+    }
+}
