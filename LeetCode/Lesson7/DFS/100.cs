e503f7be9e4d7c0e2c9bf0b355e3284ed65bb447
Minh Tri <42665212+Mtris7@users.noreply.github.com>
2021-12-14 22:29:44 +0700

    add new

---
+++ LeetCode/Lesson7/DFS/100.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Lesson7.DFS
+{
+    class _100
+    {
+        //https://leetcode.com/problems/same-tree/
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null) return true;
+            if (p == null || q == null || p.val != q.val) return false;
+
+            return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+        }
+    }
+}
---
+++ LeetCode/Lesson7/DFS/101.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class _101
+    {
+        //https://leetcode.com/problems/symmetric-tree/
+        public bool IsSymmetric(TreeNode root)
+        {
+            if (root.left == null && root.right == null) return true;
+            if (root.left == null || root.right == null) return false;
+
+            return IsSymmetric(root.left, root.right);
+        }
+        public bool IsSymmetric(TreeNode a, TreeNode b)
+        {
+            if (a == null && b == null) return true;
+            if (a == null || b == null || a.val != b.val) return false;
+
+            if ((a.left == null && b.right != null) || (a.left != null && b.right == null))
+                return false;
+            if ((a.right == null && b.left != null) || (a.right != null && b.left == null))
+                return false;
+            return IsSymmetric(a.left, b.right) && IsSymmetric(a.right, b.left);
+        }
+    }
+}
