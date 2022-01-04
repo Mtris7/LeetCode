19009d20567df50af4975b7155c40851c4be318b
Minh Tri <minhtri.win@gmail.com>
2022-01-04 17:37:31 +0700

    add

---
+++ LeetCode/lesson11/2Pointer/11.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class _11
+    {
+        //https://leetcode.com/problems/container-with-most-water/
+        public int MaxArea(int[] height)
+        {
+            int result = 0;
+            int l = 0;
+            int r = height.Length - 1;
+            while (l < r)
+            {
+                var sum = Math.Min(height[l], height[r]) * (r - l);
+                if (height[l] < height[r])
+                    l++;
+                else
+                    r--;
+                result = Math.Max(sum, result);
+            }
+            return result;
+        }
+    }
+}
