00f0282f7bb14c145db9f31b07fb0d1572de4260
Minh Tri <minhtri.win@gmail.com>
2022-01-14 14:34:20 +0700

    add 1342

---
+++ LeetCode/Lesson22/1342.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Lesson22
+{
+    class _1342
+    {
+        //https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+        public int NumberOfSteps(int num)
+        {
+            int res = 0;
+            while (num != 0)
+            {
+                res++;
+                if (num % 2 == 0)
+                {
+                    num /= 2;
+                }
+                else
+                    num--;
+            }
+            return res;
+        }
+    }
+}
