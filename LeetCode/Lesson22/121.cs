e45b06b1e542b9298efb2a3669c5d12b7b57e13c
minhtri.win@gmail.com <minhtri.win@gmail.com>
2022-01-14 22:14:14 +0700

    add 121

---
+++ LeetCode/Lesson22/121.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Lesson22
+{
+    class _121
+    {
+        //https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+        public int MaxProfit(int[] prices)
+        {
+            int min = prices[0];
+            int res = 0;
+            for (int i = 1; i < prices.Length; i++)
+            {
+                res = Math.Max(prices[i] - min, res);
+                min = Math.Min(min, prices[i]);
+            }
+            return res;
+        }
+    }
+}
