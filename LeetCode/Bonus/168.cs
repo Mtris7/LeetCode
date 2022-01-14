c624061737c25dcb6cf6f39fefb22164be2b24dc
minhtri.win@gmail.com <minhtri.win@gmail.com>
2022-01-14 22:46:07 +0700

    add 168

---
+++ LeetCode/Bonus/168.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class _168
+    {
+        //https://leetcode.com/problems/excel-sheet-column-title/
+        public string ConvertToTitle(int columnNumber)
+        {
+            string res = "";
+            while (columnNumber > 0)
+            {
+                columnNumber--;
+                res = (char)(columnNumber % 26 + 'A') + res;
+                columnNumber /= 26;
+            }
+            return res;
+        }
+    }
+}
