4cc38b8b25ddffd80ffe8b8d00c86ccb7c3d73d8
Minh Tri <minhtri.win@gmail.com>
2022-01-17 14:42:26 +0700

    add 287

---
+++ LeetCode/Bonus/287.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Bonus
+{
+    public class _287
+    {
+        //https://leetcode.com/problems/find-the-duplicate-number/
+        public int FindDuplicate(int[] nums)
+        {
+            Array.Sort(nums);
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] == nums[i - 1]) return nums[i];
+            }
+            return -1;
+        }
+    }
+}
