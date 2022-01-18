f4dc5d8d1657e28e247a47d92baf80739de1cebd
Minh Tri <minhtri.win@gmail.com>
2022-01-18 14:25:27 +0700

    add 19

---
+++ LeetCode/Lesson22/19.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class _19
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            var dummy = new ListNode();
+            dummy.next = head;
+            var fast = dummy;
+            var slow = dummy;
+            while (n >= 0)
+            {
+                fast = fast.next;
+                n--;
+            }
+            while (fast != null)
+            {
+                fast = fast.next;
+                slow = slow.next;
+            }
+            slow.next = slow.next.next;
+            return dummy.next;
+        }
+    }
+}
