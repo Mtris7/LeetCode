8b7529f5f4702f43eca33125c060aa3831420722
Minh Tri <minhtri.win@gmail.com>
2022-01-19 17:24:54 +0700

    add 530

---
+++ LeetCode/Lesson07/DFS/530.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Lesson07.DFS
+{
+    class _530
+    {
+        //https://leetcode.com/problems/minimum-absolute-difference-in-bst/
+        int res = int.MaxValue;
+        public int GetMinimumDifference(TreeNode root)
+        {
+            DFS(root, int.MaxValue, int.MaxValue);
+            return res;
+        }
+        void DFS(TreeNode root, int min, int max)
+        {
+            int left = Math.Abs(root.val - min);
+            int right = Math.Abs(max - root.val);
+            res = Math.Min(Math.Min(left, right), res);
+            if (root.left != null)
+                DFS(root.left, min, root.val);
+            if (root.right != null)
+                DFS(root.right, root.val, max);
+        }
+    }
+}
