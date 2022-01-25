e1dcf6e8850169d2f67e373251235c287c51ca63
Minh Tri <minhtri.win@gmail.com>
2022-01-25 15:15:01 +0700

    open close in priciple

---
+++ LeetCode/DesignPattern/SOLID/ProductFilter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.DesignPattern.SOLID
+{
+    //Normal
+    public class ProductFilter
+    {
+        public static IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
+        {
+            foreach (var p in products)
+            {
+                if (p.size == size)
+                    yield return p;
+            }
+        }
+        public static IEnumerable<Product> FilterByColor(IEnumerable<Product> products, Color color)
+        {
+            foreach (var p in products)
+            {
+                if (p.color == color)
+                    yield return p;
+            }
+        }
+        public static IEnumerable<Product> FilterByColorAndSize(IEnumerable<Product> products, Color color, Size size)
+        {
+            foreach (var p in products)
+            {
+                if (p.color == color && p.size == size)
+                    yield return p;
+            }
+        }
+    }
+}
