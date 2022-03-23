c95c7b11d84246a95b05d3fe875ad4fe7e8ef56a
minhtri.win@gmail.com <minhtri.win@gmail.com>
2022-03-23 08:58:36 +0700

    add 23/03/2022

---
+++ LeetCode/Bonus/991.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Bonus
+{
+    class _991
+    {
+        public int BrokenCalc(int startValue, int target)
+        {
+            int result = 0;
+
+            while (target > startValue)
+            {
+                result++;
+
+                if (target % 2 == 1)
+                    target++;
+                else
+                    target /= 2;
+            }
+
+            return result + startValue - target;
+        }
+    }
+}
