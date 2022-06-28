0afc16267d1b7ff42d8ef1b938be1becc6c0fea9
SkyAceNghiem <minhtri.win@gmail.com>
2022-06-28 10:27:15 +0700

    add 1647

---
+++ LeetCode/Bonus/1647.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Bonus
+{
+    public class _1647
+    {
+        public int MinDeletions(string s)
+        {
+            int res = 0;
+            if (s == null || s.Length == 0) return 0;
+
+            var hash = new HashSet<int>();
+            var arr = new int[26];
+
+            foreach (var c in s)
+                arr[c - 'a']++;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                while (hash.Contains(arr[i]) && arr[i] > 0)
+                {
+                    arr[i]--;
+                    res++;
+                }
+                hash.Add(arr[i]);
+            }
+            return res;
+        }
+    }
+}
